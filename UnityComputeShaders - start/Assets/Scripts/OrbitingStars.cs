77899c287be21df8c79b741c35b84a057f303e98
Floris Groen <floris.groen@dephion.com>
2022-10-14 10:37:56 +0200

    First commit

---
+++ UnityComputeShaders - start/Assets/Scripts/OrbitingStars.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitingStars : MonoBehaviour
+{
+    public int starCount = 17;
+    public ComputeShader shader;
+
+    public GameObject prefab;
+
+    int kernelHandle;
+    uint threadGroupSizeX;
+    int groupSizeX;
+    
+    Transform[] stars;
+    ComputeBuffer resultBuffer;
+    Vector3[] output;
+    
+    void Start()
+    {
+        kernelHandle = shader.FindKernel("OrbitingStars");
+        shader.GetKernelThreadGroupSizes(kernelHandle, out threadGroupSizeX, out _, out _);
+        groupSizeX = (int)((starCount + threadGroupSizeX - 1) / threadGroupSizeX);
+
+        resultBuffer = new ComputeBuffer(starCount, sizeof(float) * 3);
+        shader.SetBuffer(kernelHandle, "Result", resultBuffer);
+        output = new Vector3[starCount];
+
+        stars = new Transform[starCount];
+        for (int i = 0; i < starCount; i++)
+        {
+            stars[i] = Instantiate(prefab, transform).transform;
+        }
+    }
+
+    void Update()
+    {
+        shader.SetFloat("time", Time.time);
+        shader.Dispatch(kernelHandle, groupSizeX, 1, 1);
+
+        resultBuffer.GetData(output);
+
+        for (int i = 0; i < stars.Length; ++i)
+        {
+            stars[i].localPosition = output[i];
+        }
+    }
+
+    private void OnDestroy()
+    {
+        resultBuffer.Dispose();
+    }
+}
